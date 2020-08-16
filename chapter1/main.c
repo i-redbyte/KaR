3752966edcccb8aeed8e7dfa2ba58d088f76ba24
Ilya Sokolov <developer.sokolov@gmail.com>
2020-08-16 22:01:25 +0300

    Resolved tasks 1.6 - 1.8

--- chapter1/main.c
+++ chapter1/main.c
@@ -2,12 +2,16 @@
 void task_1_3();
 void task_1_4();
 void task_1_5();
+void task_1_6();
+void task_1_8();
 
 int main(){
     printf("Hello, C world!\n");
-    task_1_3();
-    task_1_4();
-    task_1_5();
+    // task_1_3();
+    // task_1_4();
+    // task_1_5();
+    // task_1_6();
+    task_1_8();
     return 0;
 }
 /*
@@ -46,4 +50,21 @@ void task_1_5(){
     printf("Inverse table: \n");
     for (int fahr = 300; fahr >= 0; fahr -= 20) 
         printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));  
+}
+
+void task_1_6(){
+    int c;
+    printf("EOF = %d \n", EOF); // TASK 1.7
+    while ((c = getchar()) != EOF) 
+        putchar(c);
+}
+
+void task_1_8(){
+    int c, tabCount = 0, spaceCount = 0, newlineCount = 0;
+    while ((c = getchar()) != EOF){
+        if ( c == '\n') ++newlineCount;
+        if ( c == '\t') ++tabCount;
+        if ( c == ' ') ++spaceCount;
+    }
+    printf("Lines: %2d, Tabs: %2d, Spaces: %2d", newlineCount, tabCount, spaceCount);    
 }
