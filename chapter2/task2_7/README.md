774dbc729f3d534d4b6ac7598ed0b11bf4872bdb
Ilya Sokolov <developer.sokolov@gmail.com>
2021-06-22 16:38:05 +0300

    @solution: solution for task 2.7

---
+++ chapter2/task2_7/README.md
@@ -0,0 +1,6 @@
+# Задача(Task) 2.7 #
+
+- **(RU)** Напишите функцию invert(x, p, n), возвращающую значение x с инвертированными n битами, начиная с позиции p (остальные биты не изменяются).
+
+
+- **(EN)** Write a function invert(x,p,n) that returns x with the n bits that begin at position p inverted (i.e., 1 changed into 0 and vice versa), leaving the others unchanged.
---
+++ chapter2/task2_7/solution.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+unsigned  invert(unsigned x, int p, int n){
+    unsigned mask = (~(~0 << p)) ^ ~(~0 << p-n);
+    return ((x & ~mask) | (~x & mask));
+}
+int main(void) {
+    printf("%d\n", invert(255, 8, 8));
+    printf("%d\n", invert(84, 3, 4));
+    printf("%d\n", invert(10, 4, 4));
+    printf("0x%X\n", invert(0xFF, 8, 6));
+    printf("0x%X\n", invert(0x0A, 4, 2));
+    printf("0x%X\n", invert(0xED, 8, 6));
+    return 0;
+}
