3c55423b9f2d2ca8ffa68487322950451082973f
Ilya Sokolov <developer.sokolov@gmail.com>
2021-11-08 00:56:00 +0300

    @solution: Task 5.12

---
+++ chapter5/task12/README.md
@@ -0,0 +1,8 @@
+# Задача(Task) 5.12
+
+- **(RU)** Расширьте возможности entab и detab таким образом, чтобы при обращении вида
+  entab -m +n "стопы" табуляции начинались с m-й позиции и выполнялись через каждые n позиций. Разработайте удобный
+  для пользователя вариант поведения программы по умолчанию (когда нет никаких аргументов).
+
+
+- **(EN)** Extend entab and detab to accept the shorthand entab -m +n to mean tab stops every n columns, starting at column m. Choose convenient (for the user) default behavior.
---
+++ chapter5/task12/entab_solution.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TABSIZE 4
+
+int main(int argc, char *argv[]) {
+    int c, pos, i;
+    int tabsize[2] = {TABSIZE, TABSIZE};
+
+    while (--argc > 0 && (**++argv == '-' || **argv == '+')) {
+        switch (**argv) {
+            case '-':
+                if ((pos = atoi(*argv + 1)) > 0) {
+                    tabsize[0] = pos;
+                }
+                break;
+            case '+':
+                if ((pos = atoi(*argv + 1)) > 0) {
+                    tabsize[1] = pos;
+                }
+                break;
+        }
+    }
+    pos = 0;
+    i = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            while (pos < tabsize[i]) {
+                putchar(' ');
+                ++pos;
+            }
+            pos = 0;
+            if (i < 1) i++;
+        } else {
+            putchar(c);
+            if (c == '\n') {
+                pos = 0;
+                i = 0;
+            } else {
+                ++pos;
+
+                if (pos == tabsize[i]) {
+                    pos = 0;
+                    if (i < 1) i++;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
